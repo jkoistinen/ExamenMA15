660afcf2cd670908ad0bc3285b6b9dd75c00395d
jk <jk>
2016-08-29 09:57:58 +0200

    added google-services.json and example code

--- app/src/main/java/com/jk/examenma15/MainActivity.java
+++ app/src/main/java/com/jk/examenma15/MainActivity.java
@@ -3,11 +3,21 @@ package com.jk.examenma15;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+        //Other setup code
+
+        Firebase myFirebaseRef = new Firebase("https://examenma15.firebaseio.com/");
+
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
+
     }
 }
