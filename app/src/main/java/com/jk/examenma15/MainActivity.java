a830d61bedfbc9372e69f484c557e5527c95bc84
jk <jk>
2016-08-29 10:49:23 +0200

    added new activity and some example code in mainactivity

--- app/src/main/java/com/jk/examenma15/MainActivity.java
+++ app/src/main/java/com/jk/examenma15/MainActivity.java
@@ -2,10 +2,21 @@ package com.jk.examenma15;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +30,41 @@ public class MainActivity extends AppCompatActivity {
 
         myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
 
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+
+                updateTextView(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
+
+    public void updateTextView(String value){
+
+        TextView textview = (TextView) findViewById(R.id.textview);
+        textview.setText(value);
+
+    }
+
+
+
+
 }
